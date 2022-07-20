0ee65825eceb94fb5e08539df003b7abd9286aa0
TRAVIS YOU (ITA-ISDC-ISD-OOCLL/ZHA) <YOUTR@OOCL.COM>
2022-07-20 16:18:18 +0800

    feat: finish the 'practice05'

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,22 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int classNumber = -1;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int classNumber) {
+        super(name, age);
+        this.classNumber = classNumber;
+    }
+
+    public int getKlass() {
+        return this.classNumber;
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (this.classNumber != -1 ? String.format("Class %d.", this.classNumber) : "No Class.");
+    }
 }
