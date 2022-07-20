6375ed6280621510ea5a5ebae0bc678cd5466bcc
TRAVIS YOU (ITA-ISDC-ISD-OOCLL/ZHA) <YOUTR@OOCL.COM>
2022-07-20 16:31:43 +0800

    feat: finish the 'practice06'

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -1,4 +1,17 @@
 package practice06;
 
 public class Klass {
+    private int classNumber;
+
+    public Klass(int classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public int getNumber() {
+        return this.classNumber;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", this.classNumber);
+    }
 }
--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Student {
+import sun.dc.pr.PRError;
+
+public class Student extends Person {
+    private int classNumber = -1;
+    private Klass klass;
+    public Student(String name, int age, int classNumber) {
+        super(name, age);
+        this.classNumber = classNumber;
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+        this.classNumber = klass.getNumber();
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + String.format(" I am a Student. I am at Class %d.", this.classNumber);
+    }
 }
