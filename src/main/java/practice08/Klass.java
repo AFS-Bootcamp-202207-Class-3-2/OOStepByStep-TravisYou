700d30c2893aec93f9b57f6989ac7433e8a41051
TRAVIS YOU (ITA-ISDC-ISD-OOCLL/ZHA) <YOUTR@OOCL.COM>
2022-07-20 16:55:16 +0800

    feat: finish the 'practice08'

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,40 @@
 package practice08;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int classNumber;
+    private Student leader;
+    private List<Student> members;
+
+    public Klass(int classNumber) {
+        members = new ArrayList<Student>();
+        this.classNumber = classNumber;
+    }
+
+    public int getNumber() {
+        return this.classNumber;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", this.classNumber);
+    }
+
+    public void assignLeader(Student student) {
+        if(!this.members.contains(student)) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        this.leader = student;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        members.add(student);
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,43 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int classNumber = -1;
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int classNumber) {
+        super(name, age);
+        this.classNumber = classNumber;
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+        this.classNumber = klass.getNumber();
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+        this.classNumber = klass.getNumber();
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (this.classNumber != -1 ? String.format("Class %d.", this.classNumber) : "No Class.");
+    }
+
+    public String introduceWith(Student student) {
+        return super.introduce() + " I am a Teacher. I" + (this.classNumber != student.getKlass().getNumber() ? " don't" : "") + " teach " + student.getName() + ".";
+    }
 }
